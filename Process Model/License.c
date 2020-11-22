02975f24024f95c30ab42c29dcc0f43d6d54efa0
mc97-stack <matthew.cheung97@icloud.com>
2020-11-22 17:16:22 +0000

    Licensing details.

---
+++ Process Model/License.c
@@ -0,0 +1,33 @@
+//
+//  License.c
+//  Process Model (Cross-Platform)
+//
+//  Created by Matthew Cheung on 18/11/2020.
+//  Copyright © 2020 Matthew Cheung. All rights reserved.
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or 
+//  any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+void License()
+{
+    printf("GNU GENERAL PUBLIC LICENSE\nVersion 3, 29 June 2007\nCopyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>\nEveryone is permitted to copy and distribute verbatim copies\nof this license document, but changing it is not allowed.\n");
+    
+    printf("\nFull license details can be found at https://github.com/mc97-stack/IJTB-m2UY-7iPC-D0FN/blob/master/LICENSE\n");
+    fflush(stdout);
+}
