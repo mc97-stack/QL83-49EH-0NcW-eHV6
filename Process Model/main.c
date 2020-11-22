ec688d91d1b9785f66356ee9e07bb19da08b90b8
mc97-stack <matthew.cheung97@icloud.com>
2020-11-22 16:51:49 +0000

    Initial Commit

---
+++ Process Model/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Process Model
+//
+//  Created by Matthew Cheung on 22/11/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
